2c9d68d805719cb5518c5ef89b84d162f7661f36
hcweigand10 <henryweigand10@gmail.com>
2023-03-31 16:40:16 -0700

    cols-2

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -55,7 +55,7 @@ const Gallery = () => {
           <Loading/>
         </div>
       ) : null}
-      {data ? <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">{art}</div> : null}
+      {data ? <div className="grid grid-cols-1 md:grid-cols-2">{art}</div> : null}
     </div>
   );
 };
