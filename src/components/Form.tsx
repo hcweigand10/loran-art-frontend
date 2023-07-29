b78b0dbbbfbacfc7081d61b1f7cf4753a1ee2bbd
hcweigand10 <henryweigand10@gmail.com>
2023-07-29 15:19:25 -0700

    text -> number

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -530,7 +530,7 @@ const Form = (props: formProps) => {
                   </label>
                   <div className="mt-2">
                     <input
-                      type="number"
+                      type="text"
                       name="linkUrl"
                       id="linkUrl"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -549,7 +549,7 @@ const Form = (props: formProps) => {
                   </label>
                   <div className="mt-2">
                     <input
-                      type="number"
+                      type="text"
                       name="linkText"
                       id="linkText"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
