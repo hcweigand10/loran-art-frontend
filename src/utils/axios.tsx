83c2ea25abeff0ae937f14c4d268b443624f5efb
hcweigand10 <henryweigand10@gmail.com>
2023-08-19 15:24:14 -0700

    parsing bugs

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -2,8 +2,8 @@ import axios from "axios"
 
 
 const galleryAPI = axios.create({
-  // baseURL: "https://lovely-fashion-hare.cyclic.app",
-  baseURL: "http://localhost:3001",
+  baseURL: "https://lovely-fashion-hare.cyclic.app",
+  // baseURL: "http://localhost:3001",
   timeout: 15000,
 })
 
