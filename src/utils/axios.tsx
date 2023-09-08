2a53c75e72892fbb10232a4c7531d84a47db6021
hcweigand10 <henryweigand10@gmail.com>
2023-09-08 10:37:30 -0700

    back to cyclic

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -2,8 +2,8 @@ import axios from "axios"
 
 
 const galleryAPI = axios.create({
-  baseURL: "https://loran-art-backend-81883890d96e.herokuapp.com/",
-  // baseURL: "https://lovely-fashion-hare.cyclic.app",
+  // baseURL: "https://loran-art-backend-81883890d96e.herokuapp.com/",
+  baseURL: "https://lovely-fashion-hare.cyclic.app",
   // baseURL: "http://localhost:3001",
   timeout: 15000,
 })
